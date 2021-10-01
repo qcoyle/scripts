13d2d6b9573d266180c7d66c8e8619716a77c55c
qcoyle <quinn.t.coyle@gmail.com>
2021-09-30 20:57:14 -0500

    update script

--- convert_markdown_to_html.py
+++ convert_markdown_to_html.py
@@ -3,12 +3,12 @@
 import markdown
 from markdown.extensions.toc import TocExtension
 
-f = open("markdown.md") 
+f = open("Downloads/markdown.md") 
 md_text = f.read()
 
 html = markdown.markdown(md_text, extensions=[TocExtension(toc_depth="2-5", title='Table of Contents:')])
 print("Markdown converted to html.")
 
-with open("index.html", 'w') as f:
+with open("Downloads/index.html", 'w') as f:
     f.write(html)
     f.close
