8e16d4d78171f23db5d5dde8c389b2668d08479a
qcoyle <quinn.t.coyle@gmail.com>
2021-10-07 17:29:39 -0500

    head gets kicked out, now have to get tail

--- nodejs/clean-links.js
+++ nodejs/clean-links.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-function getIndicies(file, substring) {
+function run(file, substring) {
     var substringLength = substring.length;
     if (substringLength == 0) {
         return [];
@@ -8,18 +8,15 @@ function getIndicies(file, substring) {
     var startIndex = 0,
         index, indicies = [];
 
-    console.log(file.indexOf(substring, startIndex))
-
     while ((index = file.indexOf(substring, startIndex)) > -1) {
-        indicies.push(index);
-        startIndex = index + substringLength
+        file = file.substr(0, index) + file.substr(index + substring.length)
+            // console.log(file)
     }
-    return indicies;
+    return file;
 }
 
 fs.readFile('files/index.html', encoding = "utf8", function(err, data) {
     if (err) throw err;
-    // console.log(data);
-    indicies = getIndicies(data, "https://www.google.com/url?q=");
 
+    console.log(run(data, "https://www.google.com/url?q="))
 });
