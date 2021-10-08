4c9c856fe3a474b3af748acbfe0a84e503cb2f93
qcoyle <quinn.t.coyle@gmail.com>
2021-10-08 05:51:32 -0500

    added terminal input

--- nodejs/clean-links.js
+++ nodejs/clean-links.js
@@ -1,23 +1,22 @@
+#!/usr/bin/env node
+
 var fs = require("fs");
+const inquirer = require("inquirer");
 
 function run(file, substring) {
-    var substringLength = substring.length;
-    if (substringLength == 0) {
-        return [];
-    }
     var startIndex = 0,
-        index, indicies = [];
+        index;
 
     while ((index = file.indexOf(substring, startIndex)) > -1) {
 
-        // Clean out head 
-        file = file.substr(0, index) + file.substr(index + substring.length);
-
-        // Clean out tail
+        file = removeHead(index, file, substring);
         file = removeEnd(index, file);
-
     }
     return file;
+}
+
+function removeHead(index, file, substring) {
+    return (file.substr(0, index) + file.substr(index + substring.length));
 }
 
 function removeEnd(index, file) {
@@ -26,16 +25,26 @@ function removeEnd(index, file) {
     return file.substring(0, startIndex) + file.substring(endIndex);
 }
 
-fs.readFile('files/index.html', encoding = "utf8", function(err, data) {
-    if (err) throw err;
-    file = run(data, "https://www.google.com/url?q=");
-    console.log(file);
+var questions = [{
+    type: 'input',
+    name: 'name',
+    message: "Enter file to clean (/Users/qtcoyle/Downloads/index.html)"
+}];
 
-    // Write out the file
-    fs.writeFile("/Users/qtcoyle/Downloads/cleaned-links-index.html", file, err => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-    })
+inquirer.prompt(questions).then(answers => {
+    console.log(`File entered is ${answers['name']}`)
+
+    fs.readFile(answers['name'], encoding = "utf8", function(err, data) {
+        if (err) throw err;
+        file = run(data, "https://www.google.com/url?q=");
+        console.log(file);
+
+        // Write out the file
+        fs.writeFile("/Users/qtcoyle/Downloads/cleaned-links-index.html", file, err => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        })
+    });
 });
