992de729232d76faf6f14f6e267b58979fa3aeb9
qcoyle <quinn.t.coyle@gmail.com>
2021-10-14 15:27:25 -0500

    add module exports

--- nodejs/read-file-names.js
+++ nodejs/read-file-names.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
 
 // Everything is synchronous
-var filenames = fs.readdirSync("./files/photos");
-console.log("Hello world 1");
-console.log(filenames); // Outputs an array of the file names
-filenames.forEach((file) => {
-    console.log(`File name is ${file}`);
-});
-console.log("Hello world 2");
+function run() {
+    var filenames = fs.readdirSync("./files/photos");
+    console.log("Hello world 1");
+    console.log(filenames); // Outputs an array of the file names
+    filenames.forEach((file) => {
+        console.log(`File name is ${file}`);
+    });
+    console.log("Hello world 2");
+
+    return filenames;
+}
+
+module.exports = run();
