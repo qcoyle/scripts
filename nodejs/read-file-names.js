37e6a580bc5a52bcad002be51cde8c20cdb41fa8
qcoyle <quinn.t.coyle@gmail.com>
2021-10-16 09:31:19 -0500

    add script

--- nodejs/read-file-names.js
+++ nodejs/read-file-names.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 // Everything is synchronous
 function run() {
     var filenames = fs.readdirSync("./files/photos");
-    console.log("Hello world 1");
-    console.log(filenames); // Outputs an array of the file names
+    // console.log("Hello world 1");
+    // console.log(filenames); // Outputs an array of the file names
     filenames.forEach((file) => {
-        console.log(`File name is ${file}`);
+        // console.log(`File name is ${file}`);
     });
-    console.log("Hello world 2");
+    // console.log("Hello world 2");
 
     return filenames;
 }
