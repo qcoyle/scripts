da16693ef330a519a0f0e16e1117d71a625d5180
qcoyle <quinn.t.coyle@gmail.com>
2021-10-21 06:30:48 -0500

    add to file

--- nodejs/read-file.js
+++ nodejs/read-file.js
@@ -1,6 +1,17 @@
-var fs = require("fs");
+const fs = require("fs");
 
 fs.readFile('files/index.html', encoding = "utf8", function(err, data) {
     if (err) throw err;
     console.log(data);
 });
+
+// Alternate way using a separate callback function:
+let readDataCallback = (err, data) => {
+    if (err) {
+        console.log(`Something went wrong: ${err}`);
+    } else {
+        console.log(`Provided file contained: ${data}`);
+    }
+};
+
+fs.readFile('files/index.html', 'utf-8', readDataCallback);
