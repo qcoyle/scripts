1958fe1c8ebba08045d53cef16e24bed226072d9
qcoyle <quinn.t.coyle@gmail.com>
2021-10-21 06:35:12 -0500

    Add to file

--- nodejs/terminal-input.js
+++ nodejs/terminal-input.js
@@ -1,4 +1,4 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer'); // Requires npm install inquirer
 
 var questions = [{
     type: 'input',
@@ -10,3 +10,16 @@ inquirer.prompt(questions).then(answers => {
     console.log(`Hi ${answers['name']}!`)
     console.log(answers["name"]);
 })
+
+
+// // How to do it with stdin. Comment out above if using this
+// console.log("What is your name? ")
+// process.stdin.on('data', (userInput) => {
+//     let input = userInput.toString()
+//     console.log(`Hello ${input}`)
+// });
+
+// // Returns:
+// // What is your name? 
+// // Quinn // <-- The user terminal input
+// // Hello Quinn
